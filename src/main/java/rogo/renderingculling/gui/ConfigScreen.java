cb8a7fef1019d6ee8845e66ec106cb468a2111c3
RogoShum <1269314290@qq.com>
2024-05-05 23:34:53 +0800

    support iris 1.7.0

--- src/main/java/rogo/renderingculling/gui/ConfigScreen.java
+++ src/main/java/rogo/renderingculling/gui/ConfigScreen.java
@@ -75,7 +75,10 @@ public class ConfigScreen extends Screen {
                 .color(bgColor, bgColor, bgColor, bgAlpha)
                 .uv(u(right - 1), v(top - 1)).endVertex();
         RenderSystem.setShaderTexture(0, Minecraft.getInstance().getMainRenderTarget().getColorTextureId());
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
         BufferUploader.drawWithShader(bufferbuilder.end());
+
         bgAlpha = 1.0f;
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
         bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
@@ -87,7 +90,7 @@ public class ConfigScreen extends Screen {
                 .color(bgColor, bgColor, bgColor, bgAlpha).endVertex();
         bufferbuilder.vertex(right, top, 0.0D)
                 .color(bgColor, bgColor, bgColor, bgAlpha).endVertex();
-        RenderSystem.enableBlend();
+
         RenderSystem.blendFunc(GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR, GlStateManager.DestFactor.ZERO);
         BufferUploader.drawWithShader(bufferbuilder.end());
         RenderSystem.defaultBlendFunc();
